8990b752ae3bb64f005c583a6e9a39474da3e634
Yi <125733585+zygame1314@users.noreply.github.com>
2024-10-05 23:35:47 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/zygame1314/live2d_api@latest/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/zygame1314/live2d_api@1.0.0/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
